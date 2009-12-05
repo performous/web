46fe07037884601e2fea6fa99cf742ee8b5273dd
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2009-12-05 11:15:44 +0100

    Enhanced deploy script / Added non-generated files

--- htdocs-source/deploy.sh
+++ htdocs-source/deploy.sh
@@ -1,7 +1,26 @@
 #/bin/sh
 
-GITPATH=~/src/performous-web
-SITEPATH=~/public_html/performous
+if test $# -ne 1 ; then
+	echo "-!- Please choose a profile"
+	exit
+fi
+
+case $1 in
+	tronic)
+		GITPATH=~/src/performous-web
+		SITEPATH=~/public_html/performous
+		RSH=sh
+		;;
+	yoda)
+		GITPATH=~/git/web
+		SITEPATH=root@www:/pub/www/performous/
+		RSH=ssh
+		;;
+	*)
+		echo "-!- \"$1\" is not a valid profile"
+		exit
+		;;
+esac
 
 echo ">>> Updating the GIT repository"
 git pull ${GITPATH} master || exit 1
@@ -10,10 +29,10 @@ echo ">>> Rebuilding and installing the website"
 
 TEMPPATH=`mktemp -d`
 ${GITPATH}/webconv/webconv-x86-linux.bin ${GITPATH}/htdocs-source ${TEMPPATH} &&
-rm -rf ${SITEPATH} &&
-mv ${TEMPPATH} ${SITEPATH} &&
+rsync --rsh ${RSH} --archive --verbose ${TEMPPATH}/* ${SITEPATH} &&
+rsync --rsh ${RSH} --archive --verbose ${GITPATH}/htdocs-binary/* ${SITEPATH} &&
 echo ">>> INSTALLATION SUCCESSFUL" ||
 echo "-!- INSTALLATION FAILED"
 
-rm -rf ${TEMPPATH}
+echo ">>> Please run 'rm -rf \"${TEMPPATH}\"' (script will never do an rm -rf)"
 
