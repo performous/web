8b4b50f65c088ee5d5f37949e4e34d3affa6457e
Lasse Karkkainen <tronic+ndrm@trn.iki.fi>
2010-12-05 20:17:05 +0100

    Cleanup and fix showcase scroll on Firefox

---
+++ htdocs-binary/showcase.js
@@ -0,0 +1,14 @@
+var img = Math.floor(Math.random() * 20);
+var anim = 0;
+function showcaseScroll() {
+	var e = document.getElementById('showcase');
+	var pos = 128 * (img + 0.5 * (1 - Math.cos(Math.PI * anim / 30)));
+	e.style.backgroundPosition = Math.round(-pos) + "px 0";
+	var timeout = 3500;
+	if (anim > 0) timeout = 16;
+	if (anim == 30) { anim = -1; ++img; }
+	++anim;
+	setTimeout('showcaseScroll()', timeout);
+}
+showcaseScroll();
+
